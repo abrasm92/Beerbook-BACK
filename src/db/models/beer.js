09afd0439f2d901b6502b1903c772ea92165653b
Abraham Saavedra <abrasm92@gmail.com>
2022-06-01 19:31:18 +0200

    added beerSchema

---
+++ src/db/models/beer.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const beerSchema = new Schema({
+  name: { type: String },
+  brewery: { type: String },
+  style: { type: String },
+  degrees: { type: Number },
+  ibu: { type: Number, default: null },
+  country: { type: String },
+  description: { type: String, default: null },
+  image: { type: String },
+  owner: { type: Schema.Types.ObjectId },
+});
+
+const Beer = model("Beer", beerSchema, "beers");
+
+module.exports = Beer;
