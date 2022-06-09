1837c3d5d594dc8335fb28a5da6c78f3b292170d
Abraham Saavedra <abrasm92@gmail.com>
2022-06-09 19:10:37 +0200

    refactor auxiliar functions

--- src/utilities/getBeerByIbu/getBeersByIbu.js
+++ src/utilities/getBeerByIbu/getBeersByIbu.js
@@ -1,46 +1,16 @@
 const getAllNumbersIbu = require("./getAllNumbersIbu");
 
 const getBeersByIbu = (value) => {
-  let allIbuBeerValue = [value];
+  const allIbuBeerValue = [value];
   let ibu;
-  switch (value) {
-    case "0":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "10":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "20":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "30":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "40":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "50":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "60":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "70":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "80":
-      allIbuBeerValue.push(getAllNumbersIbu(value));
-      return allIbuBeerValue;
-    case "90":
-      allIbuBeerValue = [value];
-      for (ibu = 1; ibu < 11; ibu += 1) {
-        allIbuBeerValue.push(+value + ibu);
-      }
-      return allIbuBeerValue;
-    default:
-      allIbuBeerValue = [value];
-      return allIbuBeerValue;
+  if (value === "90") {
+    for (ibu = 1; ibu < 11; ibu += 1) {
+      allIbuBeerValue.push(+value + ibu);
+    }
+    return allIbuBeerValue;
   }
+  allIbuBeerValue.push(getAllNumbersIbu(value));
+  return allIbuBeerValue;
 };
 
 module.exports = getBeersByIbu;
